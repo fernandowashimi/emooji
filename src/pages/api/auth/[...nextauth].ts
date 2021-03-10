c0cd743e22917b8b2744aa85f00a4115e4fef140
Fernando Shinji <fernandowashimi@gmail.com>
2021-03-10 19:56:07 -0300

    💎 add prisma adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import NextAuth from 'next-auth';
+import Adapters from 'next-auth/adapters';
 import Providers from 'next-auth/providers';
 
+import prisma from '@/lib/prisma';
+
 const options = {
-  site: process.env.SITE || process.env.LOCALHOST,
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_ID || '',
       clientSecret: process.env.GOOGLE_SECRET || '',
     }),
   ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
 };
 
 export default (req: NextApiRequest, res: NextApiResponse<any>) => NextAuth(req, res, options);
